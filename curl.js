adac83cc22b153261694ffb44b2a24b58e466c06
Arom Jhee <aromjhee@gmail.com>
2020-04-28 13:31:49 -0700

    got fetching the ASCII art to work

--- curl.js
+++ curl.js
@@ -1,3 +1,28 @@
-const fetch = require('fetch');
+const fetch = require('node-fetch');
 const fs = require('fs');
+const { program } = require('commander');
 
+program
+    .option('-o, --output <string>', 'file in which to store the fetched content')
+    .option('-f, --fetch', 'fetch the url body and print')
+
+const opts = program.parse(process.argv);
+// ********************
+// console.log(opts);
+// url under args property
+// can get value of output arguments 
+// ********************
+const output = opts.output; // this gives output.txt
+const url = opts.args[0]; // this gives the url
+// console.log(output, url)
+// ********************
+
+if (url) {
+    fetch(url)
+        .then((res) => res.text())
+        .then((body) => console.log(body))
+        .catch((reason) => {
+            console.log('WARNING: Only absolute URLs are supported')
+            console.log(`curl: (6) Could not resolve host: ${url}`)
+        })
+}
