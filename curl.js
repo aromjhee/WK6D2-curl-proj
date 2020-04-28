7e1dca763656d835d7b09ac56dfcf196d0efd2fe
Arom Jhee <aromjhee@gmail.com>
2020-04-28 14:14:32 -0700

    got curl.js -o command to work

--- curl.js
+++ curl.js
@@ -5,6 +5,7 @@ const { program } = require('commander');
 program
     .option('-o, --output <string>', 'file in which to store the fetched content')
     .option('-f, --fetch', 'fetch the url body and print')
+    .option('-H, --header', 'set headers of the request')
 
 const opts = program.parse(process.argv);
 // ********************
@@ -17,12 +18,31 @@ const url = opts.args[0]; // this gives the url
 // console.log(output, url)
 // ********************
 
-if (url) {
+if (program.output) {
     fetch(url)
-        .then((res) => res.text())
-        .then((body) => console.log(body))
-        .catch((reason) => {
+        .then(res => res.text())
+        .then(body => {
+            fs.writeFile(`./${output}`, body, err => {
+                if (err) {
+                    console.error(`curl: (23) Failed writing body`);
+                }
+            });
+        })
+        .catch(reason => {
             console.log('WARNING: Only absolute URLs are supported')
             console.log(`curl: (6) Could not resolve host: ${url}`)
         })
+}
+
+if (program.header) {
+    const myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    fetch(url, myInit)
+        .then
+
 }
